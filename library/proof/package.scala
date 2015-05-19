2328c5a74b708b82bee5e2fdff2ff29cff2c9773
Marco Antognini <antognini.marco@gmail.com>
2015-05-19 07:33:36 +0200

    add first draft of implication reasoning API

--- library/proof/package.scala
+++ library/proof/package.scala
@@ -9,11 +9,16 @@ package object proof {
   case class ProofOps(prop: Boolean) {
     def because(proof: Boolean): Boolean = proof && prop
     def implies(that: Boolean): Boolean = !prop || that
+    def fallacy: Boolean = { // TODO this is probaly not the right verb...
+      require(!prop)
+      prop
+    }
   }
 
   implicit def boolean2ProofOps(prop: Boolean): ProofOps = ProofOps(prop)
 
   def trivial: Boolean = true
+  def trivially(x: Boolean) = x // identity
 
   def by(proof: Boolean)(prop: Boolean): Boolean =
     proof && prop
@@ -33,4 +38,23 @@ package object proof {
   }
 
   implicit def any2EqReasoning[A](x: A): EqReasoning[A] = EqReasoning(x, true)
+
+  case class ImplicationReasoning[A](x: A, prop: Boolean) {
+    def ==>|(f: A => Boolean): ImplicationReasoning[A] = {
+      require(prop implies f(x))
+      ImplicationReasoning(x, f(x))
+      // ImplicationReasoning(x, prop implies f(x))    <- this is wrong because
+      //                                                  we propagate false all
+      //                                                  the way to the end
+    }
+
+    def |[B](that: ImplicationReasoning[B]): ImplicationReasoning[B] = {
+      ImplicationReasoning(that.x, this.prop implies that.prop)
+    }
+
+    def qed: Boolean = prop
+  }
+
+  implicit def any2ImplicationReasoning[A](x: A): ImplicationReasoning[A] = ImplicationReasoning(x, false)
 }
+
