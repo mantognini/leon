84fbf467c948b7061abf6ec9bd897d9876ca65f8
Sandro Stucki <sandro.stucki@gmail.com>
2015-05-22 18:04:51 +0200

    Generalizes equational reasoning to relational reasoning. Needs testing.

--- library/proof/package.scala
+++ library/proof/package.scala
@@ -5,26 +5,53 @@ import leon.annotation._
 import leon.lang._
 import scala.language.implicitConversions
 
+import leon.proof.Internal._
+
 package object proof {
 
   case class ProofOps(prop: Boolean) {
     def because(proof: Boolean): Boolean = proof && prop
     def neverHolds: Boolean = {
       require(!prop)
-      prop
+      !prop
     }
   }
 
   implicit def boolean2ProofOps(prop: Boolean): ProofOps = ProofOps(prop)
 
   def trivial: Boolean = true
-  def trivially(x: Boolean) = x // identity
 
   def by(proof: Boolean)(prop: Boolean): Boolean =
     proof && prop
 
-  case class EqReasoning[A](lhs: A, rhs: A, prop: Boolean) {
-    // Invariant: lhs == rhs has been already checked
+  /**
+   * Relational reasoning.
+   *
+   *         {
+   *           ((y :: ys) :+ x).last   ^^ _ == _ ^^| trivial         |
+   *           (y :: (ys :+ x)).last   ==| trivial         |
+   *           (ys :+ x).last          ==| snocLast(ys, x) |
+   *           x
+   *         }.qed
+   */
+  case class RelReasoning[A](x: A, prop: Boolean) {
+
+    def ^^[B](r: (A, B) => Boolean): WithRel[A, B] = WithRel(x, r, prop)
+
+    /** Short-hand for equational reasoning. */
+    def ==|(proof: Boolean): WithProof[A, A] = {
+      require(proof)
+      WithProof(x, _ == _, proof, prop)
+    }
+
+    def qed: Boolean = prop
+  }
+
+  implicit def any2RelReasoning[A](x: A): RelReasoning[A] =
+    RelReasoning(x, true)
+
+  /*
+  case class EqReasoning[A](x: A, prop: Boolean) {
     def ==|(proof: Boolean): EqReasoning[A] = {
       require(proof)
       EqReasoning(lhs, rhs, proof)
@@ -57,5 +84,5 @@ package object proof {
   }
 
   implicit def any2ImplicationReasoning[A](x: A): ImplicationReasoning[A] = ImplicationReasoning(x, false)
+   */
 }
-
