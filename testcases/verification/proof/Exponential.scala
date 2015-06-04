3dc53d0f88c7098a321ef2b0b0f2a829400366f2
Sandro Stucki <sandro.stucki@gmail.com>
2015-06-04 18:15:39 +0200

    Updates to exponential test case.

--- testcases/verification/proof/Exponential.scala
+++ testcases/verification/proof/Exponential.scala
@@ -27,7 +27,7 @@ object Exponential {
     }
   }.holds
 
-  /** The exponential function is monotone (short proof). */
+  /** The exponential function is monotone (shorter proof). */
   def monotoneShort(x: BigInt, y: BigInt): Boolean = {
     require(y >= 0 && x > 0)
     exp(x, y) > 0 because {
@@ -87,27 +87,114 @@ object Exponential {
     else if (y == 0)     1
     else if (y % 2 == 0) fastExp(x * x, y / 2)
     else                 x * fastExp(x, y - 1)
+  }
+
+  /**
+   * The two versions of the exponential function are equivalent.
+   *
+   * NOTE: Leon is able to verify most of the individual sub-goals of
+   * this correctness proof in fractions of a second using Z3-F, but
+   * often times out after >30s when trying to verify the overall
+   * post-condition (Intel core i7, Ubuntu 14.04.2 LTS, x86-64,
+   * 2015-06-04).
+   */
+  def fastExpCorrect(x: BigInt, y: BigInt): Boolean = {
+    require(y >= 0)
+    fastExp(x, y) == exp(x, y) because {
+      if      (x == 0)     trivial
+      else if (y == 0)     trivial
+      else if (y % 2 == 0) {
+        val z = y / 2; {
+          fastExp(x, y)         ==| trivial                  |
+          fastExp(x * x, z)     ==| fastExpCorrect(x * x, z) |
+          exp(x * x, z)         ==| square(x, z)             |
+          exp(x, y)
+        }.qed
+      }
+      else                 {
+        val z = (y - 1) / 2; {
+          fastExp(x, y)         ==| (y % 2 != 0)             |
+          x * fastExp(x * x, z) ==| {
+            fastExp(x * x, z)   ==| fastExpCorrect(x * x, z) |
+            exp(x * x, z)       ==| square(x, z)             |
+            exp(x, 2 * z)       ==| trivial                  |
+            exp(x, y - 1)                              }.qed |
+          x * exp(x, y - 1)     ==| (y != 0)                 |
+          exp(x, y)
+        }.qed
+      }
+    }
+  }.holds
+
+  /**
+   * The two versions of the exponential function are equivalent.
+   *
+   * NOTE: this version of the correctness proof is more verbose but
+   * easier to debug.  Leon is able to verify most of the individual
+   * sub-goals in fractions of a second using Z3-F, but often times
+   * out after >30s when trying to verify the overall post-condition
+   * (Intel core i7, Ubuntu 14.04.2 LTS, x86-64, 2015-06-04).
+   */
+  def fastExpCorrect2(x: BigInt, y: BigInt): Boolean = {
+    require(y >= 0)
+    fastExp(x, y) == exp(x, y) because {
+      if      (x == 0)     trivial
+      else if (y == 0)     trivial
+      else if (y % 2 == 0) {
+        val z = y / 2
+        check { fastExp(x, y)     == fastExp(x * x, z)                                   } &&
+        check { fastExp(x * x, z) == exp(x * x, z)     because fastExpCorrect2(x * x, z) } &&
+        check { exp(x * x, z)     == exp(x, y)         because square(x, z)              }
+      }
+      else                 {
+        val z = (y - 1) / 2;
+        check { fastExp(x, y)         == x * fastExp(x * x, z) because (y % 2 != 0)      } &&
+        check { x * fastExp(x * x, z) == x * exp(x, y - 1)     because {
+          check { fastExp(x * x, z)   == exp(x * x, z) because fastExpCorrect2(x * x, z) } &&
+          check { exp(x * x, z)       == exp(x, 2 * z) because square(x, z)              } &&
+          check { exp(x, 2 * z)       == exp(x, y - 1)                                   }
+        }                                                                                } &&
+        check { x * exp(x, y - 1)     == exp(x, y) because (y != 0) }
+      }
+    }
+  }.holds
+
+  /**
+   * A more efficient function for computing exponentials.
+   *
+   * NOTE: this version of fastExp incorporates the correctness proof
+   * directly into the post condition.  Leon is sometimes able to
+   * verify the post-condition in seconds using Z3-F, but often times
+   * out after >30s (Intel core i7, Ubuntu 14.04.2 LTS, x86-64,
+   * 2015-06-04).
+   */
+  def fastExp2(x: BigInt, y: BigInt): BigInt = {
+    require(y >= 0)
+    if      (x == 0)     0
+    else if (y == 0)     1
+    else if (y % 2 == 0) fastExp2(x * x, y / 2)
+    else                 x * fastExp2(x, y - 1)
   } ensuring { res =>
     res == exp(x, y) because {
       if      (x == 0)     trivial
       else if (y == 0)     trivial
       else if (y % 2 == 0) {
         val z = y / 2; {
-          res                   ==| trivial                 |
-          fastExp(x * x, z)     ==| trivial /* ind. hyp. */ |
-          exp(x * x, z)         ==| square(x, z)            |
+          res                    ==| trivial                 |
+          fastExp2(x * x, z)     ==| trivial /* ind. hyp. */ |
+          exp(x * x, z)          ==| square(x, z)            |
           exp(x, y)
         }.qed
       }
       else                 {
         val z = (y - 1) / 2; {
-          res                   ==| (y % 2 != 0)            |
-          x * fastExp(x * x, z) ==| {
-            fastExp(x * x, z)   ==| trivial /* ind. hyp. */ |
-            exp(x * x, z)       ==| square(x, z)            |
-            exp(x, 2 * z)       ==| trivial                 |
-            exp(x, y - 1)                             }.qed |
-          x * exp(x, y - 1)     ==| (y != 0)                |
+          res                    ==| (y % 2 != 0)            |
+          x * fastExp2(x * x, z) ==| {
+            fastExp2(x * x, z)   ==| trivial /* ind. hyp. */ |
+            exp(x * x, z)        ==| square(x, z)            |
+            exp(x, 2 * z)        ==| trivial                 |
+            exp(x, y - 1)                              }.qed |
+          x * exp(x, y - 1)      ==| (y != 0)                |
           exp(x, y)
         }.qed
       }
